e94179c603b7935fca9fb158f188fa4b4190c580
ekaterinady96 <ched_ric@mail.ru>
2020-06-19 15:31:29 +0300

    Выполнила домашнюю работу 3

---
+++ hw_3_task_2.py
@@ -0,0 +1,4 @@
+def my_func(name = 'Ivan', surname = 'Ivanov', year_of_birth = '1960', city = 'Ivanovo', email = 'ivanov@mail.ru', phone_number = '89851234567'):
+    print(name, surname, year_of_birth, city, email, phone_number)
+
+my_func('Ekaterina', 'Dyzhina', '1996', 'Moscow', 'ched_ric@mail.ru', '89160728118')
